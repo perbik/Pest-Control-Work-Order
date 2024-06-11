ab6eee4902bc36390c60e279873d0c3a3f770d7d
Fervicmar <fervicmardlagman@gmail.com>
2024-06-11 11:59:36 +0800

    Update App.jsx

--- Pest-Control-Work-Order/src/App.jsx
+++ Pest-Control-Work-Order/src/App.jsx
@@ -1,26 +1,11 @@
+import NavBarCompomenet from "./components/NavBarCompomenet";
+
 function App() {
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    console.log(event.target.elements.fname.value);
-    console.log(event.target.elements.lname.value);
-  };
 
   return (
     <>
-      <form className="w-25" onSubmit={handleSubmit}>
-        <div className="mb-3">
-          <input type="text" name="fname" placeholder="Enter first name..." className="form-control" />
-        </div>
-        <div className="mb-3">
-          <input type="text" name="lname" placeholder="Enter last name..." className="form-control" />
-        </div>
-        <div className="mb-3">
-          <button className="btn btn-success" type="submit">
-            Submit Form
-          </button>
-        </div>
-      </form>
+      <NavBarCompomenet />
     </>
   );
 }
