0f20096357fcade1d33a546a2f9c2bdf541b71da
Fervicmar <fervicmardlagman@gmail.com>
2024-07-08 22:40:18 +0800

    Update ProductItem

--- frontend/src/components/ProductItem/ProductItem.jsx
+++ frontend/src/components/ProductItem/ProductItem.jsx
@@ -1,11 +1,35 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./ProductItem.css";
 import { assets } from "../../assets/assets";
+import { StoreContext } from "../../context/StoreContext";
 const ProductItem = ({ id, name, price, description, image }) => {
+  const { cartItems, addToCart, removeFromCart } = useContext(StoreContext);
   return (
     <div className="product-item">
       <div className="product-item-container">
         <img className="product-item-img" src={image} alt="" />
+        {!cartItems[id] ? (
+          <img
+            className="add"
+            onClick={() => addToCart(id)}
+            src={assets.add_icon_white}
+            alt=""
+          />
+        ) : (
+          <div className="product-item-counter">
+            <img
+              onClick={() => removeFromCart(id)}
+              src={assets.remove_icon}
+              alt=""
+            />
+            <p>{cartItems[id]}</p>
+            <img
+              onClick={() => addToCart(id)}
+              src={assets.add_icon_green}
+              alt=""
+            />
+          </div>
+        )}
       </div>
       <div className="product-item-info">
         <p className="product-name">{name}</p>
