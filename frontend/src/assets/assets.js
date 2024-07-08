1b769b41a8ee983a7692918bfa4de46f3fc799cc
Fervicmar <fervicmardlagman@gmail.com>
2024-07-08 11:22:46 +0800

    Update assets

---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,34 @@
+// icons
+import cart_icon from "./cart_icon.png";
+import search_icon from "./search_icon.png";
+
+// products
+import cirrus from "./cirrus.png";
+import delatacide from "./delatacide.png";
+import fendona from "./fendona.png";
+import optigard from "./optigard.png";
+import oxyfly from "./oxyfly.png";
+import permacap from "./permacap.png";
+import pounce from "./pounce";
+import resigen_ec from "./resigen_ec.png";
+import storm_secure_wax_bait from "./storm_secure_wax_bait.png";
+import sumilarv from "./sumilarv.png";
+
+// logo
+import uno_pest_co from "./uno_pest_co.svg";
+
+export const assets = {
+  cart_icon,
+  search_icon,
+  cirrus,
+  delatacide,
+  fendona,
+  optigard,
+  oxyfly,
+  permacap,
+  pounce,
+  resigen_ec,
+  storm_secure_wax_bait,
+  sumilarv,
+  uno_pest_co,
+};
--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -1,30 +1,46 @@
-import React, { useState } from 'react'
-import './NavBar.css'
-import searchIcon from '../../assets/search_icon.png';
-import cartIcon from '../../assets/cart_icon.png';
+import React, { useState } from "react";
+import "./NavBar.css";
+import searchIcon from "../../assets/search_icon.png";
+import cartIcon from "../../assets/cart_icon.png";
+import { assets } from "../../assets/assets";
 
 const NavBar = () => {
-
-    const [menu,setMenu] = useState("Home");
+  const [menu, setMenu] = useState("Home");
 
   return (
-    <div className='navbar'>
+    <div className="navbar">
       <h2>Uno Pest Co.</h2>
-      <ul className='navbar-menu'>
-        <li onClick={()=>setMenu("Home")} className={menu==="Home"?"active":""}>Home</li>
-        <li onClick={()=>setMenu("Menu")} className={menu==="Menu"?"active":""}>Menu</li>
-        <li onClick={()=>setMenu("About")} className={menu==="About"?"active":""}>About</li>
+      <ul className="navbar-menu">
+        <li
+          onClick={() => setMenu("Home")}
+          className={menu === "Home" ? "active" : ""}
+        >
+          Home
+        </li>
+        <li
+          onClick={() => setMenu("Menu")}
+          className={menu === "Menu" ? "active" : ""}
+        >
+          Menu
+        </li>
+        <li
+          onClick={() => setMenu("About")}
+          className={menu === "About" ? "active" : ""}
+        >
+          About
+        </li>
       </ul>
       <div className="navbar-right">
-        <img src={searchIcon} alt=''/>
+        <img src={assets.uno_pest_co} alt="" />
+        <img src={assets.searchIcon} alt="" />
         <div className="navbar-search-icon">
-          <img src={cartIcon} alt=''/>
+          <img src={assets.cart_icon} alt="" />
           <div className="dot"></div>
         </div>
         <button>sign in</button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
