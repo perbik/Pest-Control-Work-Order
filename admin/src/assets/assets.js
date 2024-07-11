7ecf203c9201dd6376558c942a5f6e5516844ea0
henry <henryjamescarlos@gmail.com>
2024-07-11 10:54:45 +0800

    inital commit

---
+++ admin/src/assets/assets.js
@@ -0,0 +1,17 @@
+import add from './add.png'
+import dashboard from './dashboard.png'
+import orders from './orders.png'
+import products from './products.png'
+import profile from './profile.png'
+import unopest_logo from './unopest_logo.png'
+import upload from './upload.png'
+
+export const assets = {
+    add,
+    dashboard,
+    orders,
+    products,
+    profile,
+    unopest_logo,
+    upload
+}
